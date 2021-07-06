d8f920480236b882a03027aff7388b44ba92cf3e
hung8695 <hung8695@gmail.com>
2021-07-06 15:05:22 +0700

    hungvippromax tao class User

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,23 @@
 package model;
 
 public class User {
+    private String username;
+    private String password;
+    private String gender;
+    private int  dob;
+    private String email;
+    private int phone;
+    private String address;
+
+    public User(){}
+
+    public User(String username, String password, String gender, int dob, String email, int phone, String address) {
+        this.username = username;
+        this.password = password;
+        this.gender = gender;
+        this.dob = dob;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
 }
